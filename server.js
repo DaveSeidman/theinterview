f4b094583fc02d28bba5f214c84ff3d4cbfe45ad
dave seidman <dave.seidman@b-reel.com>
2018-03-20 11:19:11 -0400

    add video check

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const apiai = require('apiai');
 
 const apiaiClient = apiai('402cae09c92b4d228c90a026f090b97e');
 const express = require('express');
+const intents = require('./src/intents.json');
+const fs = require('fs');
+const chalk = require('chalk');
 
 const app = express();
 
@@ -26,3 +29,19 @@ app.get('/apiai/nlp/:text', (req, res) => {
 
 app.listen(8000, () => { console.log('listening on port 8000'); });
 
+const videoCheck = () => {
+  console.log(' ');
+  console.log('verifying video files:');
+  console.log('---------------------------------------');
+  Object.keys(intents).forEach((intent) => {
+    const videos = intents[intent];
+    videos.forEach((video) => {
+      const path = `./src/videos/${video}.mp4`;
+      const exists = fs.existsSync(path);
+      console.log(chalk[exists ? 'green' : 'red'](path, exists ? '✓' : 'x'));
+      // console.log(video, fs.existsSync());
+    });
+  });
+};
+
+videoCheck();
